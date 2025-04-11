3d3b181ad087eea035ace035a9270391e1a81fb9
hussam <hussam>
2025-04-11 22:14:25 +0500

    New files

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
-        cout<<"Not a Leap Year 2 ";
+
+          if(charachter=='0'||charachter=='1'||charachter=='2'||charachter=='3'||charachter=='4'||charachter=='5'||charachter=='6'||charachter=='6'||charachter=='7'||charachter=='8'||charachter=='9'){
+        cout<<"Digit"<<endl;
+        a=false;
+    }else{
+
+    
+        for(int i=1;i<=26;i++){
+            if(charachter==UpperCases[i]){
+cout<<"UpperCase"<<endl;
+a=false;
+            }
+            else if(charachter==LowerCase[i]){
+cout<<"Lower Case"<<endl;
+a=false;
+            }
+      
+        }}
